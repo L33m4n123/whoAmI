c8e5e46b7054364634ceddf13f67d78d604e64bd
Patrick Lehmann <lehmann.s.patrick@gmail.com>
2017-06-04 13:17:02 +0200

    Test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.all("*", (req, res) => {
-	console.log(req);
+	console.log("res._headers >>>>>>>" + JSON.stringify(resp._headers));
 	res.send("...");
 });
 
