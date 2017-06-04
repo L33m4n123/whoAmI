a7d590f6dd723bf5079e7d1218da7dda9716bb41
Patrick Lehmann <lehmann.s.patrick@gmail.com>
2017-06-04 13:30:04 +0200

    Test2

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.all("*", (req, res) => {
-	console.log(req.connection);
-	console.log(req.connection.remoteAdress);
-	res.send("...");
+app.get('/', function (req, res) {
+  res.render('index', {basedir: __dirname});
+});
+
+app.get('/whoami/', function (req, res) {
+  	
+  	var os = req.header('user-agent').split(')')[0].split('(')[1];
+  	var language = req.header('accept-language').split(',')[0];
+  	var ip = req.header('x-forwarded-for');
+  
+	res.send({
+		"ipaddress": ip,
+	    "language": language,
+	    "software": os
+	});
 });
 
 app.listen(app.get('port'), function() {
